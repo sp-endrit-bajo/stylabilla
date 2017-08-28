5d18609fec9eee8af19997ac4946e75ce6e6829e
Katarina Dabo <katarina@usabilla.com>
2017-08-28 10:56:57 +0200

    docs(navigation): update docs navigation structure

---
+++ docs-builder/custom/custom.js
@@ -0,0 +1,5 @@
+module.exports = function(Handlebars) {
+    Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+        return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
+    });
+};
